270239531f15615ed12d2bf8cd3dcb861c479b07
CaioPedras <caio.vp@gmail.com>
2022-06-20 13:47:59 -0300

    Bonus+BugFix

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
 import express, { json } from "express";
 import cors from "cors";
 const app = express();
-console.log("oiiii");
+
 app.use(cors());
 app.use(json());
 let users = [];
 let tweets = [];
 app.post("/sign-up", (req, res) => {
   const body = req.body;
+  if (!body.username || !body.avatar) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
   const newUser = { username: body.username, avatar: body.avatar };
   users.push(newUser);
-  res.send("ok");
+  res.status(201).send("OK");
 });
+
 app.post("/tweets", (req, res) => {
   const body = req.body;
-  const user = users.find((item) => item.username === body.username);
+  const { user } = req.headers;
+  const activeUser = users.find((item) => item.username === user);
+  if (!user || !body.tweet) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
+
   const newTweet = {
-    username: body.username,
+    username: user,
     tweet: body.tweet,
-    avatar: user.avatar,
+    avatar: activeUser.avatar,
   };
   tweets.push(newTweet);
-  res.send("ok");
+  res.status(201).send("OK");
 });
+
 app.get("/tweets", (req, res) => {
-  const numberOfTweets = 10;
-  const tweetsOrder = [...tweets].reverse();
-  if (tweetsOrder.length > 10) {
-    const tweetsDisplayed = [];
-    for (let i = 0; i < numberOfTweets; i++) {
-      tweetsDisplayed.push(tweetsOrder[i]);
-    }
-    res.send(tweetsDisplayed);
-  } else {
-    res.send(tweetsOrder);
+  const { page } = req.query;
+  if (!page || page < 1) {
+    res.status(400).send("Informe uma página válida");
+    return;
   }
+  const numberOfTweets = 10;
+  const start = (page - 1) * numberOfTweets;
+  const end = page * numberOfTweets;
+  const tweetsOrder = [...tweets].reverse();
+  res.send([...tweetsOrder].splice(start, end));
+});
+
+app.get("/tweets/:username", (req, res) => {
+  const { username } = req.params;
+  const userTweets = tweets.filter((tweet) => tweet.username === username);
+  res.send([...userTweets].reverse());
 });
 
 app.listen(5000);
